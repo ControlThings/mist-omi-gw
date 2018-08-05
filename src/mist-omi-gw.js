d7b20a7b1c36da4f0e9ef3d5c10879ad0e629d05
Jan Nyman <jan.nyman@controlthings.fi>
2018-08-05 10:47:51 +0300

    Basic bi-directional function, but there are significant bugs

--- src/mist-omi-gw.js
+++ src/mist-omi-gw.js
@@ -9,6 +9,11 @@ var pathBody = 'Your/Path/Things/';
 var omiClient = new OmiClient(host);
 
 var once = false;
+var once2 = false;
+var peers = new Array();
+var endpointTypes = new Array();
+var mistValueCache = new Array();
+var omiValueCache = new Array();
 
 omiClient.once('ready', function() {
     console.log("OmiClient connected to "+ host +'.');
@@ -61,6 +66,7 @@ function followCb(err, data) {
 
 function OmiNode() {
     var mist = new Mist({ name: 'MistApi', corePort: 10001 }); // defaults: coreIp: '127.0.0.1', corePort: 9094
+
     
     mist.request('signals', [], (err, data) => {
         //console.log(err, data[0]);
@@ -72,6 +78,11 @@ function OmiNode() {
         }
    
         if (data[0] && data[0] === "peers") {
+
+            if (once2) {
+                return;
+            }
+            once2 = true;
             mist.request('listPeers', [], (err, data) => {
                 for (var i in data) {
                     /* Each of the peers (the data[i]) will be used in an async function. Ensure variable 'peer' atomicity by putting it in an in-line function and giving data[ı] as the parameter */
@@ -82,43 +93,107 @@ function OmiNode() {
                                 return;
                             }
                             var name = data;
-                            console.log("Name", name);
-                            /* We now know the name of the Mist device, let's create the InfoItems that correspond to the device's Mist endpoints */
-                            mist.request('mist.control.model', [peer], (err, data) => {
-                                console.log(name, "model:", err, data);
-                                /* Iterate through the endpoints and ensure the corresponding InfoItems on the OMI node */
-                                for (var ep in data) {
-                                    console.log("Endpoint on " + name + ":", ep, data[ep].type);
-                                    if (ep === 'mist') {
-                                        console.log("(skipping)" + ep);
-                                    }
+                            console.log("Name", data);
+                           
+                            ((name, peer) => {
+                                /* We now know the name of the Mist device, let's create the InfoItems that correspond to the device's Mist endpoints */
+                                mist.request('mist.control.model', [peer], (err, data) => {
+                                    console.log(name, "model:", err, data);
                                     var path = pathBody + name;
-                                    switch (data[ep].type) {
-                                        case "string":
-                                        omiClient.write(path, ep, "");
-                                        break;
-                                        case "bool":
-                                        omiClient.write(path, ep, false);
-                                        break;
-                                        case "int":
-                                        case "float":
-                                        omiClient.write(path, ep, 0.0);
-                                        break;
-                                        case "invoke":
-                                        console.log("Note: invocable endpoints are not currently exposed to OmiNode");
-                                        break;
-                                        default:
-                                        console.log("Unknown Mist endpoint type:", data[ep].type);
+                                    /* Associate OMI path with the peer */
+                                    if (peers[path] === peer) {
+                                        return;
                                     }
-                                }
-                                /* The endpoints have now been ensured, start following to get updates from Mist */
-                                mist.request('mist.control.follow', [peer], (err, data) => {
-                                    var ep = data['id'];
-                                    var value = data['data'];
-                                    console.log("OMI write", path, value);
-                                    omiClient.write(path, ep, value);
+                                    peers[path] = peer;
+
+                                    /* Iterate through the endpoints and ensure the corresponding InfoItems on the OMI node */
+                                    for (var ep in data) {
+                                        console.log("Endpoint on " + name + ":", ep, data[ep].type);
+                                        if (ep === 'mist') {
+                                            console.log("(skipping)" + ep);
+                                        }
+                                        endpointTypes[path+ep] = data[ep].type;
+                                        
+                                        switch (data[ep].type) {
+                                            case "string":
+                                            omiClient.write(path, ep, "");
+                                            break;
+                                            case "bool":
+                                            omiClient.write(path, ep, false);
+                                            break;
+                                            case "int":
+                                            case "float":
+                                            omiClient.write(path, ep, 0.0);
+                                            break;
+                                            case "invoke":
+                                            console.log("Note: invocable endpoints are not currently exposed to OmiNode");
+                                            break;
+                                            default:
+                                            console.log("Unknown Mist endpoint type:", data[ep].type);
+                                        }
+                                    }
+                                    
+                                    
+                                    /* The endpoints have now been ensured, start following to get updates from Mist */
+                                    mist.request('mist.control.follow', [peer], (err, data) => {
+                                        console.log("follow", data);
+                                        if (err) {
+                                            console.log("follow error", data);
+                                            return;
+                                        }
+
+                                        if (!data) {
+                                            console.log("follow data:", data);
+                                            return;
+                                        }
+                                        var ep = data['id'];
+                                        var value = data['data'];
+                                        console.log("OMI write", path, ep, value);
+                                        if (omiValueCache[path+ep] != value) {
+                                            omiClient.write(path, ep, value);
+                                            omiValueCache[path+ep] = value;
+                                        }
+                                        mistValueCache[path+ep] = value;
+                                    });
+                                
+                                    /* Subscribe to the OMI info items */
+                                    omiClient.subscribe(path, null, {}, function(infoItemPath, data, opts) {
+                                        console.log("OMI Subscribe:", infoItemPath, data, opts, peers[infoItemPath]);
+                                        var mistEndpoint = infoItemPath.substring(infoItemPath.lastIndexOf("/")+1);
+                                        var path = infoItemPath.substring(0, infoItemPath.lastIndexOf("/")); //key to peers in order to find the peer
+
+
+
+                                        var value;
+                                        switch (endpointTypes[path+mistEndpoint]) {
+                                            case 'bool':
+                                            console.log("we know it is a bool");
+                                            value = data === "true"? true : false; 
+                                            break;
+                                            default:
+                                            console.log("unknown Mist type for", infoItemPath);
+                                        }
+
+                                        omiValueCache[path+mistEndpoint] = value;
+                                        if (mistValueCache[path+mistEndpoint] != value) {
+                                            console.log("mist write");
+                                            mist.request('mist.control.write', [peers[path], mistEndpoint, value], (err, data) => {
+                                                console.log("Wrote to Mist peer", peers[path]['rsid'], "ep", mistEndpoint, err, value);
+                                                if (!err) {
+                                                    mistValueCache[path+mistEndpoint] = value;
+                                                }
+                                            });
+                                        }
+                                        
+
+                                        
+                                        
+                                    });
+                                
+                                    
                                 });
-                            });
+                            }) (name, peer);
+                            
                         });
                     }) (data[i]);
                 }
